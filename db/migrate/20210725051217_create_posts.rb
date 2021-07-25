e0aa6a254f7fbde5dbf7858124efbdf819162379
EC2 Default User <ec2-user@ip-172-31-28-26.ap-northeast-1.compute.internal>
2021-07-25 06:48:21 +0000

    set up bootstrap4 cdn

--- db/migrate/20210725051217_create_posts.rb
+++ db/migrate/20210725051217_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
       t.string :title
-      t.txet :content
+      t.text :content
 
       t.timestamps
     end
