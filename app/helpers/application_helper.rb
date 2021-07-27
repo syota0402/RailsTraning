83f204092d9489ad6a36368f7c713eb7743e3615
EC2 Default User <ec2-user@ip-172-31-28-26.ap-northeast-1.compute.internal>
2021-07-27 11:40:21 +0000

    フラッシュメッセージを作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+    # キーに対応した背景色を呼び出す
+    def bs4_bgcolor_for(flash_key)
+        case flash_key
+        when "success"
+            "alert-success"
+        when "error"
+            "alert-danger"
+        when "alert"
+            "alert-warning"
+        when "notice"
+            "alert-info"
+        else
+            flash_key.to_s
+        end
+    end
 end
